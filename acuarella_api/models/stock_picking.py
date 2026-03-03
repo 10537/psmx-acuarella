2dc530616da338b5b12b49b02fca791743a350af
Jose Moreno <josegregoriomoreno@gmail.com>
2026-03-03 10:16:42 -0400

    [IMP]Add tracking to api fields on picking.

--- acuarella_api/models/stock_picking.py
+++ acuarella_api/models/stock_picking.py
@@ -4,8 +4,8 @@ from odoo import fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    x_carrier_tracking_ref = fields.Char(string="Carrier Tracking Ref")
-    x_carrier_partner_name = fields.Char(string="Carrier Partner Name")
-    x_carrier_identity_document = fields.Char(string="Carrier Identity Document")
-    x_carrier_delivery_address = fields.Char(string="Carrier Delivery Address")
-    x_carrier_state = fields.Char(string="Carrier State")
+    x_carrier_tracking_ref = fields.Char(string="Carrier Tracking Ref", tracking=True)
+    x_carrier_partner_name = fields.Char(string="Carrier Partner Name", tracking=True)
+    x_carrier_identity_document = fields.Char(string="Carrier Identity Document", tracking=True)
+    x_carrier_delivery_address = fields.Char(string="Carrier Delivery Address", tracking=True)
+    x_carrier_state = fields.Char(string="Carrier State", tracking=True)
